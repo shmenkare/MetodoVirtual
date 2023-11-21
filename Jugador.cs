32cb5344ba4d26fffa8400c200c7292c3ce12c47
Orion Ra <126419073+shmenkare@users.noreply.github.com>
2023-11-20 21:56:16 -0400

    Metodo virtual

---
+++ Jugador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetodoVirtual
+{
+    class Jugador
+    {
+        public void RealizarMovida()
+        {
+            Console.WriteLine("Clase: Jugador, Metodo: RealizarMovida");
+        }
+    }
+    class Juego
+    {
+        Jugador jugador1;
+        Jugador jugador2;
+
+        public void EmpezarPartida(Jugador jug1, Jugador jug2)
+        {
+            jugador1 = jug1;
+            jugador2 = jug2;
+            jugador1.RealizarMovida();
+            jugador2.RealizarMovida();
+        }
+    }
+
+    class Bot1 : Jugador
+    {
+        public void RealizarMovida()
+        {
+            Console.WriteLine("Clase: Bot1, Metodo: RealizarMovida");
+        }
+    }
+    class Bot2 : Jugador
+    {
+        public void RealizarMovida()
+        {
+            Console.WriteLine("Clase: Bot2, Metodo: RealizarMovida");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetodoVirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jugador jug1 = new Jugador();
+            //Jugador jug2 = new Jugador();
+            //Juego j = new Juego();
+            //j.EmpezarPartida(jug1, jug2);
+
+            Bot1 jug1 = new Bot1();
+            Bot2 jug2 = new Bot2();
+            Juego j = new Juego();
+            j.EmpezarPartida(jug1, jug2);
+            jug1.RealizarMovida();
+
+            Console.ReadKey();
+
+        }
+    }
+}
